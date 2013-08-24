f55f96f8369a06e9eee473fe9fad76f3d164d3c5
Andreas Brandl <mail@andreas-brandl.de>
2013-08-24 15:40:23 +0200

    Add dropdown for current user.

--- spec/features/secure_area_spec.rb
+++ spec/features/secure_area_spec.rb
@@ -24,24 +24,21 @@ describe 'the login and signup process' do
 
   context 'logging in' do
     it 'rejects invalid credentials' do
-      subject
       login_as email, password
       expect(page).to have_content('Invalid email or password')
     end
     it 'accepts valid credentials and shows logout button' do
       User.create_user!(email: email, password: password, confirmed: true)
-      subject
       login_as email, password
-      expect(page).to have_button('Logout')
+      expect(page).to have_link('Logout')
     end
   end
 
   context 'logging out' do
     it 'destroys the session when clicking logout' do
-      subject
       User.create_user!(email: email, password: password, confirmed: true)
       login_as email, password
-      click_button('Logout')
+      click_link('Logout')
       expect(current_path).to eq(root_path)
     end
   end
--- spec/features/support/sessions.rb
+++ spec/features/support/sessions.rb
@@ -1,6 +1,7 @@
 
 def login_as(user, password)
-  within(:xpath, "//div/form[@id='new_user']") do
+  visit '/'
+  within(:xpath, "//div/form[@id='login']") do
     fill_in 'user_email', :with => user
     fill_in 'user_password', :with => password
     click_button('Sign in')
